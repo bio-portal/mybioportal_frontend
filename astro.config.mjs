16ddc4ab6125dbf70cd27b94366417f96e580506
jesse.islam@mail.mcgill.ca <jesse.islam2@ladydavis.ca>
2026-05-14 14:05:04 -0400

    v0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // Must be your exact GitHub pages domain
   site: 'https://bio-portal.github.io',
-  
-  // MUST have this exact line, starting with a slash
   base: '/mybioportal_frontend',
-  
   vite: {
     plugins: [tailwindcss()],
   },
